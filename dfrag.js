d7ff6badf90bac60ae3ab6665eb4c728c8424fe6
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-09-14 23:02:49 -0600

    Remove tdd stuff - hard to mock DOM APIs

--- dfrag.js
+++ dfrag.js
@@ -1,6 +1,16 @@
 function dfrag(type, props, children) {
   var element = document.createElement(type);
 
+
+
+  if (typeof children === 'string' || typeof children === 'number') {
+    element.textContent = children;
+  } else if (children instanceof Array) {
+    for (var i = 0; i < children.length; i++) {
+      element.appendChild(children[i]);
+    }
+  }
+
   return element;
 }
 
